ea70a6ce4e649c0ba039d420aab0dcdb1662ea68
ajlopez <webmaster@ajlopez.com>
2012-03-24 16:41:34 -0300

    using variable reference in expression, with tests

--- test/test.js
+++ test/test.js
@@ -17,9 +17,9 @@ var list = new ajlogo.List(1, null);
 assert.equal(1, list.first);
 assert.equal(null, list.rest);
 
-ctx.setProcedureReference('add', function(x, y) { return x + y; });
+ctx.setProcedure('add', function(x, y) { return x + y; });
 
-var add = ctx.getProcedureReference('add');
+var add = ctx.getProcedure('add');
 
 // Evaluate Composite Expression
 
@@ -35,6 +35,21 @@ assert.equal(9, expression.evaluate(ctx));
 expression = new ajlogo.CompositeExpression([new ajlogo.ProcedureReference('add'), 2, new ajlogo.ProcedureReference('add'), 4, 5]);
 assert.equal(11, expression.evaluate(ctx));
 
+expression = new ajlogo.CompositeExpression([new ajlogo.ProcedureReference('add'), "foo", "bar"]);
+assert.equal("foobar", expression.evaluate(ctx));
+
+ctx.setVariable('one', 1);
+ctx.setVariable('two', 2);
+
+var one = new ajlogo.VariableReference('one');
+var two = new ajlogo.VariableReference('two');
+
+assert.equal(1, one.evaluate(ctx));
+assert.equal(2, two.evaluate(ctx));
+
+expression = new ajlogo.CompositeExpression([new ajlogo.ProcedureReference('add'), one, two]);
+assert.equal(3, expression.evaluate(ctx));
+
 // Compile List
 
 list = ajlogo.compileList([1, 2, 3]);
@@ -46,3 +61,10 @@ assert.equal(3, list[2]);
 
 list = ajlogo.compileList(['add', 1, 2]);
 assert.ok(list[0] instanceof ajlogo.ProcedureReference);
+assert.equal(3, (new ajlogo.CompositeExpression(list)).evaluate(ctx));
+
+list = ajlogo.compileList(['add', '"foo', '"bar']);
+assert.ok(list[0] instanceof ajlogo.ProcedureReference);
+assert.equal("foobar", (new ajlogo.CompositeExpression(list)).evaluate(ctx));
+
+
